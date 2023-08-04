e59345d83b617d1ffb991688128df12316698d65
Rabenandrasana Tafinasoa <tafinasoa35@gmail.com>
2023-08-04 09:43:04 +0300

    init project

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -4,7 +4,9 @@ export const UserSignUpHandler = (
   req: Request,
   res: Response,
   next: NextFunction
-) => {};
+) => {
+  return res.send({ message: "user register successfully" });
+};
 export const UserSignInHandler = (
   req: Request,
   res: Response,
